d7e0349d2e58fa9d1449f55a4fa58bfa50ce5e1e
Jeydin21 <jeydin@j21.dev>
2025-05-02 23:34:36 -0500

    chore: Remove blog and update production URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://acm-wiki.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -88,12 +88,8 @@ const config: Config = {
           items: [
             {
               label: 'Docs',
-              to: '/docs/intro',
-            },
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+              to: '/docs/about',
+            }
           ],
         },
         {
